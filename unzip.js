1bfabfc8b5d2c2c16488128551bcbf6d7fa3215f
ltaoo <litaowork@aliyun.com>
2016-12-10 00:30:34 +0800

    实现获取压缩包内html文件内容

--- unzip.js
+++ unzip.js
@@ -1,5 +1,18 @@
-var zip = new require('node-zip')();
+// var zip = new require('node-zip')();
 var fs = require("fs");
-zip.file('test.txt', 'hello there');
-var data = zip.generate({base64:false,compression:'DEFLATE'});
-fs.writeFileSync('test.zip', data, 'binary');
+var TextDecoder = require('text-encoding').TextDecoder
+// zip.file('test.txt', 'hello there');
+// var data = zip.generate({base64:false,compression:'DEFLATE'});
+// fs.writeFileSync('test.zip', data, 'binary');
+let fileName = '命令行Foundation项目.html'
+fs.readFile('./data/命令行Foundation项目.md.ziw', (err, res) => {
+  if(err) console.log(err)
+  let data = res
+  var zip = new require('node-zip')(data);
+  let content = zip.files['index.html']._data;
+  console.log(content.getContent())
+  var string = new TextDecoder('utf-8').decode(content.getContent());
+  console.log(string)
+  // console.log(zip.files['index.html']); // hello there
+  // fs.writeFileSync(fileName, content.getContent(), 'utf8')
+})
